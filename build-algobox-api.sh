656531f1aef15adbaa363e5e9c30850c13fb9dd3
robertom <my@email.com>
2017-01-13 15:43:49 +0100

    travis updates

--- build-algobox-api.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -e
-gradle algobox-api:clean algobox-api:test algobox-api:shadowJar chdir=./java/algobox
-
--- build-algobox-jupyter.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-docker build -t dainco/algobox-jupyter ./python/algobox
-
