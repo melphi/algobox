5026e0f31c478102b3b0edbdd65b9fff4a1c7e0f
robertom <my@email.com>
2017-01-18 21:07:16 +0100

    gateway update

--- docker/algobox-gateway/docker-entrypoint.sh
+++ docker/algobox-gateway/docker-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+set -e
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt
+mkdir /etc/nginx/.ssl
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+	-keyout /etc/nginx/.ssl/nginx.key \
+	-out /etc/nginx/.ssl/nginx.crt \
+	-subj "/C=/ST=/L=/O=/OU=/CN=0.0.0.0"
 
 exec "$@"
 
