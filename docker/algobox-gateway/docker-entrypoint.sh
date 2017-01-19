ec0f5b992fa98c9a7e9136f24019c83c869fa11f
robertom <my@email.com>
2017-01-19 10:49:35 +0100

    gateway update

--- docker/algobox-gateway/docker-entrypoint.sh
+++ docker/algobox-gateway/docker-entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 set -e
 
-mkdir /etc/nginx/.ssl
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-	-keyout /etc/nginx/.ssl/nginx.key \
-	-out /etc/nginx/.ssl/nginx.crt \
-	-subj "/C=/ST=/L=/O=/OU=/CN=0.0.0.0"
+if [ ! -f /etc/nginx/.ssl/nginx.key ]; then
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+        -keyout /etc/nginx/.ssl/nginx.key \
+        -out /etc/nginx/.ssl/nginx.crt \
+        -subj "/C=/ST=/L=/O=/OU=/CN=0.0.0.0"
+fi
 
 if [ -n "$HTPASSWD_CONTENT" ]; then
- echo "$HTPASSWD_CONTENT" > /etc/nginx/.htpasswd
+	echo "$HTPASSWD_CONTENT" > /etc/nginx/.htpasswd
+	echo "File content "
+	cat /etc/nginx/.htpasswd
 fi
 
 exec "$@"
