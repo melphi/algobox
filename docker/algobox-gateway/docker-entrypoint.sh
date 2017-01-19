464a057f36557f62d84d7c75a586ec3de3e76489
robertom <my@email.com>
2017-01-19 11:08:22 +0100

    gateway fix

--- docker/algobox-gateway/docker-entrypoint.sh
+++ docker/algobox-gateway/docker-entrypoint.sh
@@ -6,15 +6,16 @@ if [ ! -f /etc/nginx/.ssl/nginx.key ]; then
         -keyout /etc/nginx/.ssl/nginx.key \
         -out /etc/nginx/.ssl/nginx.crt \
         -subj "/C=/ST=/L=/O=/OU=/CN=0.0.0.0"
+        chown nginx.nginx /etc/nginx/.ssl/nginx.key
+        chmod 440 /etc/nginx/.ssl/nginx.key
+        chown nginx.nginx /etc/nginx/.ssl/nginx.crt
+        chmod 440 /etc/nginx/.ssl/nginx.crt
 fi
-
-echo "HTPASSWD_CONTENT variable"
-echo "$HTPASSWD_CONTENT"
 
 if [ -n "$HTPASSWD_CONTENT" ]; then
 	echo "$HTPASSWD_CONTENT" > /etc/nginx/.htpasswd
-	echo "File content "
-	cat /etc/nginx/.htpasswd
+	chown nginx.nginx /etc/nginx/.htpasswd
+	chmod 440 /etc/nginx/.htpasswd
 fi
 
 exec "$@"
